07bc4406b1ca1e20c3706fb9be7209b234ea77fa
saledev19 <saledev19@gmail.com>
2024-01-22 12:06:35 -0800

    commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
 
-    Args:
-        a: first integer
-        b: second integer
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
