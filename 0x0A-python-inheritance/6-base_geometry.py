80b13a1d656cf2095d621c0d16a4087bb332f5b7
saledev19 <saledev19@gmail.com>
2024-03-16 21:09:01 +0000

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""initialize module"""
+
+
+class BaseGeometry:
+    """create class"""
+    def area(self):
+        raise Exception("area() is not implemented")#!/usr/bin/python3
+"""initialize module"""
+
+
+class BaseGeometry:
+    """create class"""
+    def area(self):
+        raise Exception("area() is not implemented")
